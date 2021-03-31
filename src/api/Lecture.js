f307a3d84dfecd80f08befbb40afbc3ecb550016
clastneo.study01 <clastneo.study01@gmail.com>
2021-03-31 16:50:05 +0900

    Lecture api 일부 정리

--- src/api/Lecture.js
+++ src/api/Lecture.js
@@ -1,16 +1,3 @@
-// import axios from 'axios'
-
-// export default {
-//   lectureListAll:()=>axios.get(`${process.env.VUE_APP_API_BASE}/lectures`),
-  // lectureDetail:()=>axios.get(`${process.env.VUE_APP_API_BASE}/lectures/${lectureId}`),
-  // subscribe:()=>axios.post(`${process.env.VUE_APP_API_BASE}/lecture-users/"lectureId":${lectureId},"userId":${userId}`),
-  // unSubscribe:()=>axios.delete(`${process.env.VUE_APP_API_BASE}/lecture-users/${lectureUserId}`),
-
-  
-  // list:()=>axios.get(`${process.env.VUE_APP_API_BASE}/lectures/search/stuff-sort-paging`),
-// }
-
-
 import axios from 'axios'
 
 export default {
