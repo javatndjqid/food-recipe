912900d9f4fdf1bf1c81f35416f332b6b278b256
clastneo.study01 <clastneo.study01@gmail.com>
2021-04-01 23:54:08 +0900

    path setting and build succeeded

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
+  publicPath: '',
+  // https://velog.io/@yjok/Vue-%EB%B0%B0%ED%8F%AC-%ED%9B%84-static-filecss-js...%EC%9D%84-%EB%B6%88%EB%9F%AC%EC%98%A4%EC%A7%80-%EB%AA%BB%ED%95%98%EB%8A%94-%EC%98%A4%EB%A5%98
   transpileDependencies: [
     'vuetify'
   ]
-}
+};
