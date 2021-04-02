ce54fc41813dddce03f4a574fcd0715ba151dcfa
javatndjqid <javatndjqid@gmail.com>
2021-04-02 13:38:47 +0900

    history off

--- src/api/Search.js
+++ src/api/Search.js
@@ -8,5 +8,5 @@ export default {
   recipe: (id) => axios.get(`${process.env.VUE_APP_API_SEARCH}/recipe/search/${id}`),
   count: () => axios.get(`${process.env.VUE_APP_API_SEARCH}/recipe/search/count`),
   // filter: (categoryId, page, stuffs) => axios.post(`${process.env.VUE_APP_API_BASE}/recipe/search/stuffs/${categoryId}?page=${page}`, stuffs),
-  detail: (id) => axios.get(`${process.env.VUE_APP_API_SEARCH_RECIPE}/recipes/${id}`),
+  detail: (id) => axios.get(`${process.env.VUE_APP_API_MYPAGE}/recipes/${id}`),
 }
