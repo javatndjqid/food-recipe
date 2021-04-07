9d092689dfb1903a29bd8ee3c0f02bf23a60c83a
gatakh89 <gatakh89@gmail.com>
2021-04-07 11:30:10 +0900

    lecture - Auth Setting

--- src/api/Lecture.js
+++ src/api/Lecture.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
+import request from './request'
 
 export default {
   list: () => axios.get(`${process.env.VUE_APP_API_LECTURE}/lectures`),
   detail: (id) => axios.get(`${process.env.VUE_APP_API_LECTURE}/lectures/${id}`),
-  subscribe: (lectureId) => axios.post(`${process.env.VUE_APP_API_LECTURE}/lecture-users/${lectureId}`),
-  unSubscribe: (lectureId) => axios.delete(`${process.env.VUE_APP_API_LECTURE}/lecture-users/${lectureId}`),
-  information: (lectureId) => axios.get(`${process.env.VUE_APP_API_LECTURE}/lecture-users/${lectureId}`),
+  subscribe: (lectureId) => request.post(`${process.env.VUE_APP_API_LECTURE}/lecture-users/${lectureId}`),
+  unSubscribe: (lectureId) => request.delete(`${process.env.VUE_APP_API_LECTURE}/lecture-users/${lectureId}`),
+  information: (lectureId) => request.get(`${process.env.VUE_APP_API_LECTURE}/lecture-users/${lectureId}`),
   related: (category) => axios.get(`${process.env.VUE_APP_API_LECTURE}/lectures/search/category`, {params:{keyword: category}}),
 
 
