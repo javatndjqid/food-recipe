5ba126a78f2a155d6f64475e63bd1f086e6ca4d0
gatakh89 <gatakh89@gmail.com>
2021-04-08 10:26:25 +0900

    0408

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,30 @@
 module.exports = {
   publicPath: '',
+
   // https://velog.io/@yjok/Vue-%EB%B0%B0%ED%8F%AC-%ED%9B%84-static-filecss-js...%EC%9D%84-%EB%B6%88%EB%9F%AC%EC%98%A4%EC%A7%80-%EB%AA%BB%ED%95%98%EB%8A%94-%EC%98%A4%EB%A5%98
-  transpileDependencies: [
+  "transpileDependencies": [
     'vuetify'
-  ]
+  ],
+
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'default',
+      overrideEndpoint: false,
+      region: 'ap-northeast-2',
+      bucket: 'food-recipe-project',
+      createBucket: false,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: false,
+      pluginVersion: '4.0.0-rc3',
+      uploadConcurrency: 5
+    }
+  }
 };
