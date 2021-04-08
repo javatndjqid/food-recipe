e9937bfe95a06e513391582f16e9f70f0f380bdd
javatndjqid <javatndjqid@gmail.com>
2021-04-08 14:29:29 +0900

    mobile mod

--- src/search/SearchDetail.vue
+++ src/search/SearchDetail.vue
@@ -19,139 +19,156 @@
   font-weight: normal;
   font-style: normal;
 }
+@font-face {
+  font-family: "ELAND_Nice_M";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts-20-12@1.0/ELAND_Nice_M.woff")
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
 </style>
 <template>
   <div style="color: gray">
-    <v-col cols="8" style="margin-left: auto; margin-right: auto">
-      <v-card class="mx-auto" style="padding: 20px">
-        <v-img :src="recipe.image" max-height="400px" alt="Main Image"></v-img>
+    <v-row>
+      <v-col cols="8" style="margin-left: auto; margin-right: auto">
+        <v-card class="mx-auto" style="padding: 20px">
+          <v-img
+            :src="recipe.image"
+            max-height="400px"
+            alt="Main Image"
+          ></v-img>
 
-        <div style="margin-top: 5px; display: flex; float: right">
-          <v-avatar style="flex: 1"
-            ><v-img
-              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-              alt="Avatar Image"
-          /></v-avatar>
-          <v-card-text style="flex: 20">유저 아이디</v-card-text>
-        </div>
-        <v-card-title> {{ recipe.recipeName }} </v-card-title>
-        <v-card-subtitle> {{ recipe.tip }} </v-card-subtitle>
-        <v-expand-transition>
-          <div>
-            <v-divider></v-divider>
-            <v-card-text> {{ recipe.explanation }} </v-card-text>
+          <div style="margin-top: 5px; display: flex; float: right">
+            <v-card-text style="flex: 20"
+              >작성자: {{ recipe.userNickName }}</v-card-text
+            >
           </div>
-        </v-expand-transition>
-      </v-card>
-      <v-card style="margin-top: 30px">
-        <v-card-title>재료</v-card-title>
-        <v-divider></v-divider>
-        <!-- <v-list-item>
+          <v-card-title
+            style="
+              font-family: ELAND_Nice_M;
+              white-space: nowrap;
+              overflow: hidden;
+              text-overflow: ellipsis;
+            "
+          >
+            {{ recipe.recipeName }}
+          </v-card-title>
+          <v-card-subtitle> {{ recipe.tip }} </v-card-subtitle>
+          <v-expand-transition>
+            <div>
+              <v-divider></v-divider>
+              <v-card-text> {{ recipe.explanation }} </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+        <v-card style="margin-top: 30px">
+          <v-card-title>재료</v-card-title>
+          <v-divider></v-divider>
+          <!-- <v-list-item>
           <v-list-item-title v-for="(item, i) in stuffs" :key="i"
             >{{ item.stuff }} {{ item.quentity }}</v-list-item-title
           >
         </v-list-item> -->
-        <v-list>
-          <v-list-item class="Aligner">
-            <v-list-item-action
-              style="width: 50%"
-              class="Aligner-item Aligner-item--top"
-              v-for="(items, i) in stuffs"
-              :key="i"
-            >
-              <v-simple-table dense style="width: 100%">
-                <template v-slot:default>
-                  <thead style="background-color: blue">
-                    <tr>
-                      <th class="text-left white--text">Stuff</th>
-                      <th class="text-left white--text">Quentity</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr v-for="(item, j) in items" :key="j">
-                      <td>{{ item.stuffName }}</td>
-                      <td>{{ item.quantity }}</td>
-                    </tr>
-                  </tbody>
-                </template>
+          <!-- <v-list>
+            <v-list-item class="Aligner"> -->
+          <v-row align="start" justify="center">
+            <v-col cols="12" md="4" v-for="(items, i) in stuffs" :key="i">
+              <v-simple-table dense>
+                <!-- <template v-slot:default> -->
+                <thead style="background-color: blue">
+                  <tr>
+                    <th class="white--text">Stuff</th>
+                    <th class="white--text">Quentity</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(item, j) in items" :key="j">
+                    <td>{{ item.stuffName }}</td>
+                    <td>{{ item.quantity }}</td>
+                  </tr>
+                </tbody>
+                <!-- </template> -->
               </v-simple-table>
-            </v-list-item-action>
-          </v-list-item>
-        </v-list>
-      </v-card>
-
-      <v-card
-        style="margin-top: 30px; font-weight: bold; font-family: yg-jalnan"
-        v-for="(item, i) in recipe.recipeProcedure"
-        :key="i"
-      >
-        <v-list>
-          <v-list-item>
+            </v-col>
+          </v-row>
+          <!-- </v-list-item>
+          </v-list> -->
+        </v-card>
+        <v-col
+          cols="12"
+          style="margin-top: 30px; font-weight: bold; font-family: yg-jalnan"
+          v-for="(item, i) in recipe.recipeProcedure"
+          :key="i"
+        >
+          <v-card>
+            <!-- <v-list>
+              <v-list-item> -->
             <!-- <v-list-item-action>{{ i + 1 }}</v-list-item-action> -->
-            <v-list-item-action-text style="font-size: 15pt"
-              >{{ item.recipeProcedure }}
-            </v-list-item-action-text>
-            <div style="margin-left: auto">
-              <v-list-item-action
-                ><v-img
+            <v-row align="center" justify="center">
+              <v-col cols="12" md="8">
+                <v-card-text style="font-size: 15pt">
+                  {{ item.recipeProcedure }}
+                </v-card-text>
+              </v-col>
+              <v-col cols="12" md="4">
+                <v-img
                   :src="item.recipeProcedureImage"
-                  min-width="300px"
-                  min-height="100px"
-                  style="float: right"
+                  width="350"
                   alt="Recipe Procedure Image"
-              /></v-list-item-action>
-            </div>
-          </v-list-item>
-        </v-list>
-      </v-card>
-      <v-card style="margin-top: 30px">
-        <v-card-title>판매 재료</v-card-title>
-        <v-divider></v-divider>
-        <v-list-item>
-          <v-col v-for="(item, i) in products" :key="i" cols="3">
-            <v-card
-              style="text-align: center; margin: 10px"
-              @click="navigateToMarket(item.id)"
-              flat
-            >
-              <v-img
-                :src="item.productTitleImage"
-                height="150px"
-                min-width="200px"
-                alt="Market Stuff Image"
-              />
-              <v-list-item-title style="margin: 10px; font-weight: bold">{{
-                item.name
-              }}</v-list-item-title>
-            </v-card>
-          </v-col>
-        </v-list-item>
-      </v-card>
-      <v-card style="margin-top: 30px">
-        <v-card-title>추천 강의</v-card-title>
-        <v-divider></v-divider>
-        <v-list-item>
-          <v-col v-for="(item, i) in lectures" :key="i" cols="3">
-            <v-card
-              style="text-align: center; margin: 10px"
-              @click="navigateToLecture(item.id)"
-              flat
-            >
-              <v-img
-                :src="`https://img.youtube.com/vi/${item.imageSrc}/0.jpg`"
-                height="150px"
-                min-width="200px"
-                alt="Lecture Image"
-              />
-              <v-list-item-title
-                style="text-align: center; margin: 10px; font-weight: bold"
-                >{{ item.title }}</v-list-item-title
+                />
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+        <v-card style="margin-top: 30px">
+          <v-card-title>판매 재료</v-card-title>
+          <v-divider></v-divider>
+          <v-row>
+            <v-col v-for="(item, i) in products" :key="i" cols="12" md="3">
+              <v-card
+                style="text-align: center; margin: 10px"
+                @click="navigateToMarket(item.id)"
+                flat
               >
-            </v-card>
-          </v-col>
-        </v-list-item>
-      </v-card>
-    </v-col>
+                <v-img
+                  :src="item.productTitleImage"
+                  height="150px"
+                  min-width="200px"
+                  alt="Market Stuff Image"
+                />
+                <v-list-item-title style="margin: 10px; font-weight: bold">{{
+                  item.name
+                }}</v-list-item-title>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-card>
+        <v-card style="margin-top: 30px">
+          <v-card-title>추천 강의</v-card-title>
+          <v-divider></v-divider>
+          <v-row>
+            <v-col v-for="(item, i) in lectures" :key="i" cols="12" md="3">
+              <v-card
+                style="text-align: center; margin: 10px"
+                @click="navigateToLecture(item.id)"
+                flat
+              >
+                <v-img
+                  :src="`https://img.youtube.com/vi/${item.imageSrc}/0.jpg`"
+                  height="150px"
+                  min-width="200px"
+                  alt="Lecture Image"
+                />
+                <v-list-item-title
+                  style="text-align: center; margin: 10px; font-weight: bold"
+                  >{{ item.title }}</v-list-item-title
+                >
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 <script>
@@ -203,13 +220,13 @@ export default {
       });
     },
     sliceList() {
-      let i = this.recipe.stuffRecipe.length / 3 + 1;
-      if (this.recipe.stuffRecipe.length % 3 == 0) i = i - 1;
-      this.stuffs[0] = this.recipe.stuffRecipe.slice(0, i);
+      let i = this.recipe.stuffRecipe.length / 3;
 
-      this.stuffs[1] = this.recipe.stuffRecipe.slice(i, i * 2);
+      this.stuffs[0] = this.recipe.stuffRecipe.slice(0, i + 1);
 
-      this.stuffs[2] = this.recipe.stuffRecipe.slice(i * 2);
+      this.stuffs[1] = this.recipe.stuffRecipe.slice(i + 1, i * 2 + 1);
+
+      this.stuffs[2] = this.recipe.stuffRecipe.slice(i * 2 + 1);
     },
     async getLectureData() {
       if (this.recipe.category == null) this.recipe.category = " ";
